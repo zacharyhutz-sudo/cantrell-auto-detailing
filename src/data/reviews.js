6a8e80ed0e7adbb9966ee9d40acfa89d0fed6813
Sam <sam@Sams-Mac-mini.local>
2026-02-18 09:04:04 -0500

    Add testimonials page and auto-scrolling review banner

---
+++ src/data/reviews.js
@@ -0,0 +1,62 @@
+export const reviews = [
+	{
+		stars: 5,
+		firstName: "Daniel",
+		lastInitial: "R",
+		text: "These guys do a great job detailing cars. I was told about them from a friend 3 years ago. I have taken several vehicles here and they never disappoint. I highly recommend you give these guys a try. You will want to thank me later. Greg the owner and staff are very friendly and professional. I will continue to use and recommend people to this company. Quality, consistent work, and professionalism is hard to find these days. They knock these core values out of the park!"
+	},
+	{
+		stars: 5,
+		firstName: "Rebecca",
+		lastInitial: "H",
+		text: "For car care and detailing I highly recommend this business ! I stopped by without an appointment hoping to get my car's exterior cleaned. They were able to work me in and my car has never looked so good! I appreciate the great service and how quickly my car was cleaned and the price is unbeatable. I'll definitely continue to go there in the future and will schedule an appointment. I'm glad I took a chance and stopped in. Made my day!"
+	},
+	{
+		stars: 5,
+		firstName: "Renee",
+		lastInitial: "N",
+		text: "They did a fantastic job getting stains out of my seats. I'm very happy with how great the inside and outside of my van turned out. The guys were very friendly and provided excellent customer service. I highly recommend them!"
+	},
+	{
+		stars: 5,
+		firstName: "Rob",
+		lastInitial: "S",
+		text: "I was truly amazed at the job these guys did on my 2004 Suburban! It was really dirty inside and out. The previous owner had small children and must have allowed them to eat and drink in the car as there were a lot of stains. The interior looks brand new now! They also got under the hood and it looks new! I highly recommend Cantrell's!"
+	},
+	{
+		stars: 5,
+		firstName: "Walker",
+		lastInitial: "B",
+		text: "I just had my 2016 mustang convertible detailed at Cantrell's. They made the car look like show room condition. There were two significant scratches on the hood that they took out. The price was better than I expected and better than fare. 5 Stars."
+	},
+	{
+		stars: 5,
+		firstName: "Frontal",
+		lastInitial: "L",
+		text: "My white Tacoma had black staining and acid rain spots on the windows. Greg and his crew polished it up and put on a ceramic coating and made it look brand new. Excellent work!"
+	},
+	{
+		stars: 5,
+		firstName: "Grace",
+		lastInitial: "H",
+		text: "My car shines and looks like new! Cantrell's indeed took care of all the details. I reserved and paid for detailing only the outside, but they also vacuumed the inside for me."
+	},
+	{
+		stars: 5,
+		firstName: "Samuel",
+		lastInitial: "G",
+		text: "Great experience with the folks at Cantrell's! Made a 12 year old Honda headlight look almost new! Great people who do great work!"
+	},
+	{
+		stars: 5,
+		firstName: "Kevin",
+		lastInitial: "C",
+		text: "Greg was helpful and fair prices. The guys did an awesome job on 2 of my vehicles. I definitely would go back. Thanks Cantrell's"
+	},
+	{
+		stars: 5,
+		firstName: "J",
+		lastInitial: "M",
+		text: "Hands down the best! Wish I could share a picture of my 71 SS Camaro they did for me! It was a new car!"
+	}
+];
